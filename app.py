5051a1931a020e1eda901d30546802c317f2499c
NaviCo-de <aridarrell30@gmail.com>
2025-06-04 21:46:58 +0700

    Tambah comment

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import streamlit as st
 
-def next_step():
+def next_step(): #Function ketika tombol 'Next" ditekan
     if st.session_state.p_index < len(data) - 1:
-        st.session_state.p_index += 1
+        st.session_state.p_index += 1 #Index untuk pertanyaan + 1 sehingga lanjut ke pertanyaan selanjutnya
 
-def back_step():
-    if st.session_state.p_index > 0:
-        st.session_state.p_index -= 1
+def back_step(): #Function ketika tombol 'Back' ditekan
+    if st.session_state.p_index > 0: #Ketika di pertanyaan index ke 0 atau pertanyaan pertama, ketika click tombol 'Back' pun tidak akan pindah kemana mana
+        st.session_state.p_index -= 1 #Index untuk pertanyaan - 1 sehingga kembali ke pertanyaan sebelumnya
 
-st.title("🧐WHAT IS YOUR PASSION🧐")
+st.title("🧐WHAT IS YOUR PASSION🧐") #Judul
 
-st.header("Mini quizzz")
-data = [
+st.header("Mini quizzz") #Pengantar
+data = [ #Mendeclare data yang isinya kumpulan pertanyaan dan pilihan
    {
        "pertanyaan" : "Saat nongkrong, lu lebih suka...",
        "pilihan": [
@@ -38,47 +38,48 @@ data = [
    }
 ]
 
-if "p_index" not in st.session_state:
+if "p_index" not in st.session_state: #Menggunakan st.session_state karena jika hanya variabel biasa maka akan ter reset (Setiap ada interaksi user, streamlit membaca ulang dari atas)
     st.session_state.p_index = 0
 if "jawaban_user" not in st.session_state:
-    st.session_state.jawaban_user = [None] * len(data)
+    st.session_state.jawaban_user = [None] * len(data) #Membuat list untuk menyimpan jawaban dari radio yang telah dijawab oleh user
 
 if "submitted" not in st.session_state:
     st.session_state.submitted = False
 
 index_sekarang = st.session_state.p_index
-pertanyaan_sekarang = data[index_sekarang]
+pertanyaan_sekarang = data[index_sekarang] #Mengakses pertanyaan dari list data dengan index_sekarang
 
 st.subheader(f"Pertanyaan ke-{index_sekarang+1} dari {len(data)}")
-jawaban = st.radio(
-    pertanyaan_sekarang["pertanyaan"],
-    pertanyaan_sekarang["pilihan"],
-    index = pertanyaan_sekarang["pilihan"].index(st.session_state.jawaban_user[index_sekarang])
+jawaban = st.radio( #Pilihan
+    pertanyaan_sekarang["pertanyaan"], #Judul dari pertanyaan
+    pertanyaan_sekarang["pilihan"], #Pilihan-pilihan yang tersedia, mengambil value dari key "pilihan"
+    index = pertanyaan_sekarang["pilihan"].index(st.session_state.jawaban_user[index_sekarang]) #Menyimpan jawaban user ketika next atau back pertanyaan
     if st.session_state.jawaban_user[index_sekarang] in pertanyaan_sekarang["pilihan"] else 0,
     key = f"radio_{index_sekarang}"
 )
 
-st.session_state.jawaban_user[index_sekarang] = jawaban
+st.session_state.jawaban_user[index_sekarang] = jawaban #Meng assign setiap value dari radio ke list jawaban_user
 
-col1, col2  = st.columns([1,1])
-with col1:
-    st.button("Back", on_click=back_step)
+col1, col2  = st.columns([1,1]) #Membuat column sehingga bisa kanan kiri
+with col1: #Isi dari kolom 1
+    st.button("Back", on_click=back_step) #Tombol back untuk ke pertanyaan sebelumnya
 
-with col2:
-    if index_sekarang == len(data) - 1:
-        if st.button("Submit"):
+with col2: #Isi dari kolom 2
+    if index_sekarang == len(data) - 1: #Ketika index sudah mencapai len(data) - 1 atau pertanyaan terakhir
+        if st.button("Submit"): #Mengganti tombol 'Next' dengan tombol 'Submit'
             st.session_state.submitted = True
-    else:
+    else: #Selain dari indeks terakhir, tombol tetap menjadi 'Next'
         st.button("Next", on_click=next_step)
 
-if st.session_state.submitted:
+if st.session_state.submitted: #Conditional ketika user sudah menekan tombol submit
     jawaban_user = st.session_state.jawaban_user
+    #Assign poin untuk setiap category
     programmer = 0
     uiux = 0
     data_scientist = 0
 
-    for i in range (len(jawaban_user)):
-        for pilihan in data[i]["pilihan"]:
+    for i in range (len(jawaban_user)): #Looping setiap index
+        for pilihan in data[i]["pilihan"]: #NEsted loop untuk semua 'pilihan' di data dengan indeks i
             if jawaban_user[i] == pilihan and data[i]["pilihan"].index(pilihan) == 0:
                 programmer += 1
             elif jawaban_user[i] == pilihan and data[i]["pilihan"].index(pilihan) == 1:
@@ -86,6 +87,7 @@ if st.session_state.submitted:
             elif jawaban_user[i] == pilihan and data[i]["pilihan"].index(pilihan) == 2:
                 data_scientist += 1
 
+    #Print output sesuai nilai poin tertinggi
     if programmer > uiux and programmer > data_scientist:
         st.subheader("SELAMAT KAMU OTW TIDUR 1 JAM PER HARI SEBAGAI PROGRAMMER")
     elif uiux > programmer and uiux > data_scientist:
